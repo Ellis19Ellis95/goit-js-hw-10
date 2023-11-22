4d5793a9e27a2829214ade6774c3125fdd741b29
Ellis19Ellis95 <101639151+Ellis19Ellis95@users.noreply.github.com>
2023-11-22 21:43:43 +0200

    44

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
-const apiKey = 'live_qZO7TNMdL1rg7fGC7EGhcr1xQ0BS5PeYcJYCFQPoKnJmK1M7zLIYuo7mVRMjgG7g';
+const apiKey = 'live_qZO7TNMdL1rg7fGC7EGhcr1xQ0BS5PeYcJYCFQPoKnJmK1M7zLIYuo7mVRMjgG7g'; 
 axios.defaults.headers.common['x-api-key'] = apiKey;
 
-// Функція для отримання списку порід котів
-export function fetchBreeds() {
+function fetchBreeds() {
   return axios.get('https://api.thecatapi.com/v1/breeds')
     .then(response => response.data)
     .catch(error => {
@@ -12,8 +11,7 @@ export function fetchBreeds() {
     });
 }
 
-// Функція для отримання інформації про кота за його породою
-export function fetchCatByBreed(breedId) {
+function fetchCatByBreed(breedId) {
   return axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
     .then(response => response.data)
     .catch(error => {
@@ -21,36 +19,17 @@ export function fetchCatByBreed(breedId) {
     });
 }
 
-const breedSelect = document.querySelector('.breed-select');
-const loader = document.querySelector('.loader');
-const error = document.querySelector('.error');
-const catInfo = document.querySelector('.cat-info');
-
-// Функція для створення розмітки зображення кота
-function createCatImage(url) {
-  const catImage = document.createElement('img');
-  catImage.classList.add('cat-image');
-  catImage.setAttribute('src', url);
-  catImage.setAttribute('alt', 'Cat');
-  return catImage;
-}
-
 function updateCatInfo(catData) {
   const breedName = document.querySelector('.breed-name');
   const description = document.querySelector('.description');
   const temperament = document.querySelector('.temperament');
-  const imageURL = catData[0].url;
-
+  const catImage = document.querySelector('.cat-image');
+  
   breedName.textContent = catData[0].breeds[0].name;
   description.textContent = catData[0].breeds[0].description;
   temperament.textContent = catData[0].breeds[0].temperament;
-
-  // Очистити попередні зображення
-  catInfo.innerHTML = '';
-
-  // Створення розмітки зображення кота та вставка в DOM
-  const catImg = createCatImage(imageURL);
-  catInfo.appendChild(catImg);
+  catImage.src = catData[0].url;
+  catImage.alt = 'Cat';
 }
 
 function updateBreedsList(breeds) {
@@ -65,39 +44,48 @@ function updateBreedsList(breeds) {
 }
 
 function showLoader() {
+  const loader = document.querySelector('.loader');
+  const error = document.querySelector('.error');
+
   loader.classList.remove('hidden');
   error.classList.add('hidden');
 }
 
 function showError() {
+  const loader = document.querySelector('.loader');
+  const error = document.querySelector('.error');
+
   loader.classList.add('hidden');
   error.classList.remove('hidden');
 }
 
-// Після завантаження сторінки отримаємо список порід
 window.addEventListener('DOMContentLoaded', () => {
+  const breedSelect = document.querySelector('.breed-select');
+  
   showLoader();
+
   fetchBreeds()
     .then(breeds => {
       updateBreedsList(breeds);
+      const loader = document.querySelector('.loader');
       loader.classList.add('hidden');
       breedSelect.classList.remove('hidden');
     })
     .catch(() => {
       showError();
     });
-});
 
-// Після обрання породи отримаємо інформацію про кота
-breedSelect.addEventListener('change', () => {
-  showLoader();
-  const selectedBreedId = breedSelect.value;
-  fetchCatByBreed(selectedBreedId)
-    .then(catData => {
-      updateCatInfo(catData);
-      catInfo.classList.remove('hidden');
-    })
-    .catch(() => {
-      showError();
-    });
+  breedSelect.addEventListener('change', () => {
+    showLoader();
+    const selectedBreedId = breedSelect.value;
+    fetchCatByBreed(selectedBreedId)
+      .then(catData => {
+        updateCatInfo(catData);
+        const catInfo = document.querySelector('.cat-info');
+        catInfo.classList.remove('hidden');
+      })
+      .catch(() => {
+        showError();
+      });
+  });
 });
