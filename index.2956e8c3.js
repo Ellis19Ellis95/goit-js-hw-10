de148bb8af705cd070a458fccb3124c9df9a0db2
Ellis19Ellis95 <101639151+Ellis19Ellis95@users.noreply.github.com>
2023-11-22 21:06:35 +0000

    Deploying to gh-pages from @ Ellis19Ellis95/goit-js-hw-10@50eb48460e14ab2c0621398a22c40857982b3f19 🚀

--- index.2956e8c3.js
+++
@@ -1,2 +0,0 @@
-!function(){fetchBreeds().then((function(e){console.log(e)})).catch((function(e){console.error("Error fetching breeds:",e)}));fetchCatByBreed("your_selected_breed_id_here").then((function(e){console.log(e)})).catch((function(e){console.error("Error fetching cat info:",e)}))}();
-//# sourceMappingURL=index.2956e8c3.js.map
--- index.433b4bcc.js
+++
@@ -1,2 +0,0 @@
-fetchBreeds().then((e=>{console.log(e)})).catch((e=>{console.error("Error fetching breeds:",e)}));fetchCatByBreed("your_selected_breed_id_here").then((e=>{console.log(e)})).catch((e=>{console.error("Error fetching cat info:",e)}));
-//# sourceMappingURL=index.433b4bcc.js.map
