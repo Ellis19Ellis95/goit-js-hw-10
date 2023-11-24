93148997e376fc3af044fe727db84bcd539cd615
Ellis19Ellis95 <101639151+Ellis19Ellis95@users.noreply.github.com>
2023-11-24 20:13:04 +0200

    120

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,33 +1,19 @@
+
 import axios from 'axios';
 
 const apiKey = 'live_qZO7TNMdL1rg7fGC7EGhcr1xQ0BS5PeYcJYCFQPoKnJmK1M7zLIYuo7mVRMjgG7g'; 
 
 axios.defaults.headers.common['x-api-key'] = apiKey;
 
-
 export function fetchBreeds() {
   return axios.get('https://api.thecatapi.com/v1/breeds')
     .then(response => response.data)
-    .catch(error => {
-      showError(); 
-      throw new Error(error);
-    })
-    .finally(() => {
-      hideError(); 
-    });
-}
+    };
+
 
 export function fetchCatByBreed(breedId) {
-  const apiUrl = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&api_key=${apiKey}`;
-  return fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => data)
-    .catch(error => {
-      showError(); 
-      console.error('Error fetching cat by breed:', error);
-      throw error;
-    })
-    .finally(() => {
-      hideError();
-    });
-}
+    const apiUrl = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&api_key=${apiKey}`;
+    return fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => data)
+      };
