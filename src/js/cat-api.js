2e3b9df91cf2e6685289ad0175ea9d69023f2b8c
Ellis19Ellis95 <101639151+Ellis19Ellis95@users.noreply.github.com>
2023-11-24 22:42:06 +0200

    140

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -12,7 +12,7 @@ export function fetchBreeds() {
   const errorElement = document.querySelector('.error');
   errorElement.classList.add('hidden'); 
 
-  return axios.get('https://api.hecatapi.com/v1/breeds')
+  return axios.get('https://api.thecatapi.com/v1/breeds')
     .then(response => {
       loader.classList.add('hidden'); 
       return response.data;
@@ -31,7 +31,7 @@ export function fetchCatByBreed(breedId) {
   const errorElement = document.querySelector('.error');
   errorElement.classList.add('hidden'); 
 
-  const apiUrl = `https://api.hecatapi.com/v1/images/search?breed_ids=${breedId}&api_key=${apiKey}`;
+  const apiUrl = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&api_key=${apiKey}`;
   return fetch(apiUrl)
     .then(response => {
       loader.classList.add('hidden');
