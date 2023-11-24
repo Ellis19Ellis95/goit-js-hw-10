16b66cb8ca1c69f8ca5910521460e045a3fa5b52
Ellis19Ellis95 <101639151+Ellis19Ellis95@users.noreply.github.com>
2023-11-25 00:00:01 +0200

    144

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -5,31 +5,28 @@ const apiKey = 'live_qZO7TNMdL1rg7fGC7EGhcr1xQ0BS5PeYcJYCFQPoKnJmK1M7zLIYuo7mVRM
 
 axios.defaults.headers.common['x-api-key'] = apiKey;
 
-export function fetchBreeds() {
-  const loader = document.querySelector('.loader');
-  loader.classList.remove('hidden'); 
+const loader = document.querySelector('.loader');
+const errorElement = document.querySelector('.error');
 
-  const errorElement = document.querySelector('.error');
-  errorElement.classList.add('hidden'); 
+export function fetchBreeds() {
+  loader.classList.remove('hidden');
+  errorElement.classList.add('hidden');
 
   return axios.get('https://api.thecatapi.com/v1/breeds')
     .then(response => {
-      loader.classList.add('hidden'); 
+      loader.classList.add('hidden');
       return response.data;
     })
     .catch(error => {
-      errorElement.classList.remove('hidden'); 
-      loader.classList.add('hidden'); 
+      errorElement.classList.remove('hidden');
+      loader.classList.add('hidden');
       throw new Error(error);
     });
 }
 
 export function fetchCatByBreed(breedId) {
-  const loader = document.querySelector('.loader');
-  loader.classList.remove('hidden'); 
-
-  const errorElement = document.querySelector('.error');
-  errorElement.classList.add('hidden'); 
+  loader.classList.remove('hidden');
+  errorElement.classList.add('hidden');
 
   const apiUrl = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&api_key=${apiKey}`;
   return fetch(apiUrl)
@@ -39,8 +36,8 @@ export function fetchCatByBreed(breedId) {
     })
     .then(data => data)
     .catch(error => {
-      errorElement.classList.remove('hidden'); 
-      loader.classList.add('hidden'); 
+      errorElement.classList.remove('hidden');
+      loader.classList.add('hidden');
       throw new Error(error);
     });
 }
